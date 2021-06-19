6ef766819a4a0fe500d3bf16d5dee4c76d26da4a
Diana Ware <dianasofia.ware@gmail.com>
2021-06-19 07:50:38 +0000

    Syntax updates

--- src/main.py
+++ src/main.py
@@ -167,25 +167,15 @@ def handle_login():
     return jsonify(accessToken=access_token)
 
 
+@app.route("/profile", methods=['POST'])
+def handle_profile():
+    json = request.get_json()
+    user = User.user_have_token(json["token"])
+    if user is None :
+        raise APIExeption("You don't have a token")
 
+    return jsonify(user.serialize()),200
 
-
-
-# @app.route("/profile", methods=['POST'])
-# def handle_profile():
-#     json = request.get_json()
-#     user = User.user_have_token(json["token"])
-#     if user is None :
-#         raise APIExeption("You don't have a token")
-
-#     return jsonify(user.serialize()),200
-
-@app.route("/protected", methods=["GET"])
-@jwt_required()
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
