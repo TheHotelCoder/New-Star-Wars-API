2e88f23aa2f500d90bea898be3f01a7ff36d237e
Diana Ware <dianasofia.ware@gmail.com>
2021-06-19 06:26:01 +0000

    Did everything again as the prev repository was throwing errors

--- src/main.py
+++ src/main.py
@@ -2,23 +2,152 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planets,People
 #from models import Person
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
+
+
+
+@app.route("/users",methods=['GET'])
+def all_users():
+    people = User.get_all()
+    people_dic = []
+    for person in people :
+        people_dic.append(person.serialize())
+    return jsonify(people_dic),200
+
+@app.route("/people",methods=['GET'])
+def all_people():
+    people = People.get_all()
+    people_dic = []
+    for person in people :
+        people_dic.append(person.serialize())
+    return jsonify(people_dic)
+
+@app.route("/people",methods=['POST'])
+def create_people ():
+    json = request.get_json()
+    people = People()
+    people.set_with_json(json)
+    people.db_post()
+    return jsonify(people.serialize())
+
+@app.route("/people/<int:people_id>", methods=['GET'])
+def one_people(people_id):
+    people = People.get_one_by_id(people_id)
+    people_serialized = people.serialized()
+    return jsonify(people_serialized)
+
+@app.route("/people/<int:people_id>", methods=['DELETE'])
+def delte_one_people(people_id):
+    people = People.query.get(people_id)
+    db.session.delete(people)
+    db.session.commit()
+    return jsonify(people.serialize(), "msg: User is deleted") 
+
+@app.route("/people" , methods=["DELETE"])
+def delete_all_person():
+    people = People.get_all()
+    print(people)
+
+
+@app.route("/planets", methods=["GET"])
+
+def all_planets():
+    planets = Planets.get_all()
+    planets_dic = []
+    for planet in planets :
+        planets_dic.append(planet.serialize())
+    return jsonify(planets_dic)
+
+@app.route("/planets",methods=['POST'])
+def create_planet():
+    json = request.get_json()
+    planets = Planets()
+    planets.set_with_json(json)
+    planets.db_post()
+    return jsonify(planets.serialize())
+
+@app.route("/planet/<int:planet_id>", methods=['GET'])
+def one_planet(planet_id):
+    planet = People.get_one_by_id(planet_id)
+    planet_serialized = planet.serialize()
+    return jsonify(planet_serialized) 
+
+
+@app.route("/login", methods=['POST'])
+def handle_login():
+
+    json=request.get_json()
+
+    if json is None: 
+        raise APIException("You shoulld be return a json")
+
+    if "email" not in json:
+        raise APIException("That's not an email in json")
+
+    if "password" not in json:
+        raise APIException("That's not a password in json")
+    
+    print(json["email"],json["password"])
+   
+    email = json["email"]
+    password = json["password"]
+
+    user = User.query.filter_by(email=email).one_or_none()
+
+    if user is None:
+         raise APIException("User not found")
+
+    if not user.check_password(password):
+      return jsonify("Your credentials are wrong, please try again"), 401
+
+    access_token = create_access_token(identity=user.serialize())
+    return jsonify(accessToken=access_token)
+
+
+
+
+
+
+# @app.route("/profile", methods=['POST'])
+# def handle_profile():
+#     json = request.get_json()
+#     user = User.user_have_token(json["token"])
+#     if user is None :
+#         raise APIExeption("You don't have a token")
+
+#     return jsonify(user.serialize()),200
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,169 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text, Boolean, Column
+from werkzeug.security import safe_str_cmp
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class BaseModel():
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+        
 
+    @classmethod
+    def get_one_by_id(cls,model_id):
+        return cls.query.filter_by(id = model_id).first()
+
+
+    @classmethod 
+    def delete_all(cls):
+        return cls.query.delete()
+        
+
+class User(db.Model,BaseModel):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    email= Column(String(250), nullable=False)
+    password= Column(String(250), nullable=False)
+    is_logged= Column(Boolean, default=False, nullable=False)
+    token = Column(String(250), nullable=True)
+    favorite_planets= db.relationship('Favorite_Planet', backref='user', lazy=True)
+
+
+    @staticmethod
+    def login_credentials(email,password):
+        return User.query.filter_by(email=email).filter_by(password=password).first()
+    
+    
+    def user_have_token(self,token):
+        return User.query.filter_by(token=self.token).first()
+   
+    def assign_token(self,token):
+        self.token = token
+        db.session.add(self)
+        db.session.commit()
+    
+    def check_password(self, password_param):
+        return safe_str_cmp(self.password.encode('utf-8'), password_param.encode('utf-8'))
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
+            "is_logged": self.is_logged,
+            "favorite_planets": list(map(lambda x: x.serialize(), self.favorite_planets))
             # do not serialize the password, its a security breach
         }
+
+
+class Planets(db.Model,BaseModel):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    population=Column(Integer,primary_key=False)
+    orbital_period=Column(Integer,primary_key=False)
+    rotation_period=Column(Integer,primary_key=False)
+    diameter =Column(Integer,primary_key=False)
+    favorite_planets = db.relationship('Favorite_Planet', backref='planets', lazy=True)
+    
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "population": self.population,
+            "orbital_period" :self.orbital_period,
+            "rotation_period" : self.rotation_period,
+            "diameter": self.diameter 
+            # do not serialize the password, its a security breach
+        }
+
+    
+
+        
+    def db_post(self):        
+        db.session.add(self)
+        db.session.commit()
+
+    def set_with_json(self,json):
+        self.name = json["name"]
+        self.population = json["population"]
+        self.orbital_period = json["orbital_period"]
+        self.rotation_period = json["rotation_period"]
+        self.diameter = json["diameter"]
+
+    def db_delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+        
+
+    
+
+
+
+class People(BaseModel,db.Model):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    eye_color=Column(String(250))
+    skin_color=Column(String(250))
+    gender=Column(String(250))
+    height = Column(String(250))
+    description= Column(String(250))
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color":self.eye_color,
+            "skin_color":self.skin_color,
+            "gender":self.gender,
+            "height":self.height,
+            "description":self.description
+            # do not serialize the password, its a security breach
+        }
+    
+    def db_post(self):        
+        db.session.add(self)
+        db.session.commit()
+    
+    def set_with_json(self,json):
+        self.name = json["name"]
+        self.eye_color = json["eye_color"]
+        self.skin_color = json["skin_color"]
+        self.gender = json["gender"]
+        self.height = json["height"]
+        self.description = json["description"]
+
+
+
+class Favorite_Planet(BaseModel,db.Model):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, db.ForeignKey('user.id'))
+    planet_id = Column(Integer, db.ForeignKey('planets.id'))
+    
+
+    def __repr__(self):
+        return '<Favorite_Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id":self.planet_id,
+            }
